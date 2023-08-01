00c40b8b4e750713d73d7a58fc059d2ceac727b0
RianVitor26 <rianvitorlhe@gmail.com>
2023-08-01 04:54:29 -0300

    feat: criando método remove de foods

--- menu/src/main/java/api/menu/controller/FoodController.java
+++ menu/src/main/java/api/menu/controller/FoodController.java
@@ -2,9 +2,13 @@ package api.menu.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,25 +19,43 @@ import api.menu.dtos.FoodResponseDTO;
 import api.menu.model.FoodModel;
 import api.menu.repositories.FoodRepositories;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping(value = "foods")
 public class FoodController {
 
     @Autowired
-    private FoodRepositories repository; 
+    private FoodRepositories repository;
 
-    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping
-    public List<FoodResponseDTO> listAll(){
+    public List<FoodResponseDTO> listAll() {
 
         List<FoodResponseDTO> foodsList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
-        return foodsList;   
+        return foodsList;
     }
 
     @PostMapping
-    public void create (@RequestBody FoodRequestDTO data){
+    public void create(@RequestBody FoodRequestDTO data) {
         FoodModel foodData = new FoodModel(data);
         repository.save(foodData);
         return;
     }
-}   
+
+    @PutMapping("/{id}")
+    public void update(@PathVariable Long id, @RequestBody FoodResponseDTO data) {
+        FoodModel existingFood = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Food not found"));
+
+       
+
+        repository.save(existingFood);
+    }
+
+    @DeleteMapping("/{id}")
+    public void remove(@PathVariable Long id) {
+        FoodModel existingFood = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Food not found"));
+
+        repository.delete(existingFood);
+    }
+}
